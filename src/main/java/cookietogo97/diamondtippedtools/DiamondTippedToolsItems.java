8d028bcc33bc9c60537fc34d63818b680c382805
Cookietogo97 <cookietogo97@gmail.com>
2024-09-17 21:27:59 +0200

    Added new tools to RecipeInitializer and DiamondTippedToolsItems and changed ids

--- src/main/java/cookietogo97/diamondtippedtools/DiamondTippedToolsItems.java
+++ src/main/java/cookietogo97/diamondtippedtools/DiamondTippedToolsItems.java
@@ -9,14 +9,44 @@ public class DiamondTippedToolsItems {
 	public static IronPickaxe ironPickaxe;
 	public static GoldPickaxe goldPickaxe;
 	public static SteelPickaxe steelPickaxe;
+	public static WoodHoe woodHoe;
+	public static StoneHoe stoneHoe;
+	public static IronHoe ironHoe;
+	public static GoldHoe goldHoe;
+	public static SteelHoe steelHoe;
+	public static WoodShovel woodShovel;
+	public static StoneShovel stoneShovel;
+	public static IronShovel ironShovel;
+	public static GoldShovel goldShovel;
+	public static SteelShovel steelShovel;
+	public static WoodAxe woodAxe;
+	public static StoneAxe stoneAxe;
+	public static IronAxe ironAxe;
+	public static GoldAxe goldAxe;
+	public static SteelAxe steelAxe;
 
 	ItemBuilder itemBuilder = new ItemBuilder("diamondtippedtools");
 
 	public void Initialize() {
-		woodenPickaxe = itemBuilder.build((new WoodenPickaxe("tool.pickaxe.wood.tipped", 11000)));
-		stonePickaxe = itemBuilder.build(new StonePickaxe("tool.pickaxe.stone.tipped", 11001));
-		ironPickaxe = itemBuilder.build(new IronPickaxe("tool.pickaxe.iron.tipped", 11002));
-		goldPickaxe = itemBuilder.build(new GoldPickaxe("tool.pickaxe.gold.tipped", 11003));
-		steelPickaxe = itemBuilder.build(new SteelPickaxe("tool.pickaxe.steel.tipped", 11004));
+		woodenPickaxe = itemBuilder.build((new WoodenPickaxe("tool.pickaxe.wood.tipped", 20000)));
+		stonePickaxe = itemBuilder.build(new StonePickaxe("tool.pickaxe.stone.tipped", 20001));
+		ironPickaxe = itemBuilder.build(new IronPickaxe("tool.pickaxe.iron.tipped", 20002));
+		goldPickaxe = itemBuilder.build(new GoldPickaxe("tool.pickaxe.gold.tipped", 20003));
+		steelPickaxe = itemBuilder.build(new SteelPickaxe("tool.pickaxe.steel.tipped", 20004));
+		woodHoe = itemBuilder.build((new WoodHoe("tool.hoe.wood.tipped", 20005)));
+		stoneHoe = itemBuilder.build(new StoneHoe("tool.hoe.stone.tipped", 20006));
+		ironHoe = itemBuilder.build(new IronHoe("tool.hoe.iron.tipped", 20007));
+		goldHoe = itemBuilder.build(new GoldHoe("tool.hoe.gold.tipped", 20008));
+		steelHoe = itemBuilder.build(new SteelHoe("tool.hoe.steel.tipped", 20009));
+		woodShovel = itemBuilder.build((new WoodShovel("tool.shovel.wood.tipped", 20010)));
+		stoneShovel = itemBuilder.build(new StoneShovel("tool.shovel.stone.tipped", 20011));
+		ironShovel = itemBuilder.build(new IronShovel("tool.shovel.iron.tipped", 20012));
+		goldShovel = itemBuilder.build(new GoldShovel("tool.shovel.gold.tipped", 20013));
+		steelShovel = itemBuilder.build(new SteelShovel("tool.shovel.steel.tipped", 20014));
+		woodAxe = itemBuilder.build((new WoodAxe("tool.axe.wood.tipped", 20015)));
+		stoneAxe = itemBuilder.build(new StoneAxe("tool.axe.stone.tipped", 20016));
+		ironAxe = itemBuilder.build(new IronAxe("tool.axe.iron.tipped", 20017));
+		goldAxe = itemBuilder.build(new GoldAxe("tool.axe.gold.tipped", 20018));
+		steelAxe = itemBuilder.build(new SteelAxe("tool.axe.steel.tipped", 20019));
 	}
 }
--- src/main/java/cookietogo97/diamondtippedtools/RecipeInitializer.java
+++ src/main/java/cookietogo97/diamondtippedtools/RecipeInitializer.java
@@ -26,5 +26,65 @@ public class RecipeInitializer {
 			.addInput(Item.diamond)
 			.addInput(Item.toolPickaxeSteel)
 			.create("Diamond Tipped Steel Pickaxe", DiamondTippedToolsItems.steelPickaxe.getDefaultStack());
+		RecipeBuilder.Shapeless("diamondtippedtools")
+			.addInput(Item.diamond)
+			.addInput(Item.toolHoeWood)
+			.create("Diamond Tipped Wood Hoe", DiamondTippedToolsItems.woodHoe.getDefaultStack());
+		RecipeBuilder.Shapeless("diamondtippedtools")
+			.addInput(Item.diamond)
+			.addInput(Item.toolHoeStone)
+			.create("Diamond Tipped Stone Hoe", DiamondTippedToolsItems.stoneHoe.getDefaultStack());
+		RecipeBuilder.Shapeless("diamondtippedtools")
+			.addInput(Item.diamond)
+			.addInput(Item.toolHoeIron)
+			.create("Diamond Tipped Iron Hoe", DiamondTippedToolsItems.ironHoe.getDefaultStack());
+		RecipeBuilder.Shapeless("diamondtippedtools")
+			.addInput(Item.diamond)
+			.addInput(Item.toolHoeGold)
+			.create("Diamond Tipped Gold Hoe", DiamondTippedToolsItems.goldHoe.getDefaultStack());
+		RecipeBuilder.Shapeless("diamondtippedtools")
+			.addInput(Item.diamond)
+			.addInput(Item.toolHoeSteel)
+			.create("Diamond Tipped Steel Hoe", DiamondTippedToolsItems.steelHoe.getDefaultStack());
+		RecipeBuilder.Shapeless("diamondtippedtools")
+			.addInput(Item.diamond)
+			.addInput(Item.toolShovelWood)
+			.create("Diamond Tipped Wooden Shovel", DiamondTippedToolsItems.woodShovel.getDefaultStack());
+		RecipeBuilder.Shapeless("diamondtippedtools")
+			.addInput(Item.diamond)
+			.addInput(Item.toolShovelStone)
+			.create("Diamond Tipped Stone Shovel", DiamondTippedToolsItems.stoneShovel.getDefaultStack());
+		RecipeBuilder.Shapeless("diamondtippedtools")
+			.addInput(Item.diamond)
+			.addInput(Item.toolShovelIron)
+			.create("Diamond Tipped Iron Shovel", DiamondTippedToolsItems.ironShovel.getDefaultStack());
+		RecipeBuilder.Shapeless("diamondtippedtools")
+			.addInput(Item.diamond)
+			.addInput(Item.toolShovelGold)
+			.create("Diamond Tipped Gold Shovel", DiamondTippedToolsItems.goldShovel.getDefaultStack());
+		RecipeBuilder.Shapeless("diamondtippedtools")
+			.addInput(Item.diamond)
+			.addInput(Item.toolShovelSteel)
+			.create("Diamond Tipped Steel Shovel", DiamondTippedToolsItems.steelShovel.getDefaultStack());
+		RecipeBuilder.Shapeless("diamondtippedtools")
+			.addInput(Item.diamond)
+			.addInput(Item.toolAxeWood)
+			.create("Diamond Tipped Wooden Axe", DiamondTippedToolsItems.woodAxe.getDefaultStack());
+		RecipeBuilder.Shapeless("diamondtippedtools")
+			.addInput(Item.diamond)
+			.addInput(Item.toolAxeStone)
+			.create("Diamond Tipped Stone Axe", DiamondTippedToolsItems.stoneAxe.getDefaultStack());
+		RecipeBuilder.Shapeless("diamondtippedtools")
+			.addInput(Item.diamond)
+			.addInput(Item.toolAxeIron)
+			.create("Diamond Tipped Iron Axe", DiamondTippedToolsItems.ironAxe.getDefaultStack());
+		RecipeBuilder.Shapeless("diamondtippedtools")
+			.addInput(Item.diamond)
+			.addInput(Item.toolAxeGold)
+			.create("Diamond Tipped Gold Axe", DiamondTippedToolsItems.goldAxe.getDefaultStack());
+		RecipeBuilder.Shapeless("diamondtippedtools")
+			.addInput(Item.diamond)
+			.addInput(Item.toolAxeSteel)
+			.create("Diamond Tipped Steel Axe", DiamondTippedToolsItems.steelAxe.getDefaultStack());
 	}
 }
