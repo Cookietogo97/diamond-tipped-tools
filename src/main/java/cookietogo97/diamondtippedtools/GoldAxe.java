f10830b2d06509b79e6678656e8c5d980251682f
Cookietogo97 <cookietogo97@gmail.com>
2024-09-17 21:07:44 +0200

    Added axes

---
+++ src/main/java/cookietogo97/diamondtippedtools/GoldAxe.java
@@ -0,0 +1,9 @@
+package cookietogo97.diamondtippedtools;
+
+import net.minecraft.core.item.tool.ItemToolAxe;
+
+public class GoldAxe extends ItemToolAxe {
+	public GoldAxe(String name, int id) {
+		super(name, id, ToolMaterialTipped.tipped_gold);
+	}
+}
---
+++ src/main/java/cookietogo97/diamondtippedtools/SteelAxe.java
@@ -0,0 +1,9 @@
+package cookietogo97.diamondtippedtools;
+
+import net.minecraft.core.item.tool.ItemToolAxe;
+
+public class SteelAxe extends ItemToolAxe {
+	public SteelAxe(String name, int id) {
+		super(name, id, ToolMaterialTipped.tipped_steel);
+	}
+}
