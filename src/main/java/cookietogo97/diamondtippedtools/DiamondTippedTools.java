97e07616bd1f7971178547aa0a0a1ffc428d8813
Cookietogo97 <cookietogo97@gmail.com>
2024-09-29 01:14:52 +0200

    Added RecipeGroup in DiamondTippedTools and updated version number to 1.0.1

--- src/main/java/cookietogo97/diamondtippedtools/DiamondTippedTools.java
+++ src/main/java/cookietogo97/diamondtippedtools/DiamondTippedTools.java
@@ -1,7 +1,12 @@
 package cookietogo97.diamondtippedtools;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.block.Block;
 import net.minecraft.core.data.registry.Registries;
+import net.minecraft.core.data.registry.recipe.RecipeGroup;
+import net.minecraft.core.data.registry.recipe.RecipeSymbol;
+import net.minecraft.core.data.registry.recipe.entry.RecipeEntryCrafting;
+import net.minecraft.core.item.ItemStack;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.util.GameStartEntrypoint;
@@ -34,6 +39,8 @@ public class DiamondTippedTools implements ModInitializer, GameStartEntrypoint, 
 	@Override
 	public void initNamespaces() {
 		new DiamondTippedToolsRecipeRegistries();
+		final RecipeGroup<RecipeEntryCrafting<?, ?>> WORKBENCH = new RecipeGroup<>(new RecipeSymbol(new ItemStack(Block.workbench)));
+		DiamondTippedToolsRecipeRegistries.DIAMONDTIPPEDTOOLS.register("workbench", WORKBENCH);
 		Registries.RECIPES.register("diamondtippedtools", DiamondTippedToolsRecipeRegistries.DIAMONDTIPPEDTOOLS);
 	}
 }
